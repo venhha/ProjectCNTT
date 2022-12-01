2566002207b4c67993da6d2923e1b9a2046205db
venhha123 <venh.ha@gmail.com>
2022-12-01 15:00:11 +0700

    demo2

--- bookstore/store/views.py
+++ bookstore/store/views.py
@@ -1,12 +1,46 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Books
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
 
+from .models import Books
 # Create your views here.
+
+
 def index(request):
-    #return render(request, 'index.html')
     books = Books.objects.all().values()
-    output = ""
-    for x in books:
-        output += x["book_name"]+','
-    return HttpResponse(output)
+    return render(request, 'index.html', context={'books': books})
+
+
+def add(request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
+
+
+def addrecord(request):
+    n = request.POST['book_name']
+    book = Books()
+    book.book_name = n
+    book.save()
+    return HttpResponseRedirect(reverse('index'))
+
+
+def delete(request, id):
+    book = Books.objects.get(id=id)
+    book.delete()
+    return HttpResponseRedirect(reverse('index'))
+
+
+def update(request, id):
+    book = Books.objects.get(id=id)
+    return render(request, 'update.html', context={'book': book})
+
+
+def updaterecord(request, id):
+    name = request.POST['name']
+    num = request.POST['num']
+    book = Books.objects.get(id=id)
+    book.book_name = name
+    book.stock_num = num
+    book.save()
+    return HttpResponseRedirect(reverse('index'))
