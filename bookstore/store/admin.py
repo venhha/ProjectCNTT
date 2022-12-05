fc153dfd95c825cadb28149208d24e3843b274ba
venhha123 <venh.ha@gmail.com>
2022-12-05 17:30:10 +0700

    register admin

--- bookstore/store/admin.py
+++ bookstore/store/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Author, Product, Category, Books
+from .models import *
+
 
 # Register your models here.
 admin.site.register(Author)
-admin.site.register(Product)
 admin.site.register(Category)
-admin.site.register(Books)
+admin.site.register(Product)
+
+admin.site.register(Order)
+admin.site.register(Invoice)
+admin.site.register(Customer)
